bbf9b2e5b1b0873e4cd19915a9091436dd4f85a4
AshlyOlivares <143655511+AshlyOlivares@users.noreply.github.com>
2023-11-17 20:24:10 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace traductor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtTarget = new System.Windows.Forms.TextBox();
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.cmbSourceLanguage = new System.Windows.Forms.ComboBox();
+            this.cmbTargetLanguage = new System.Windows.Forms.ComboBox();
+            this.ReadSource = new System.Windows.Forms.Button();
+            this.ReadTarget = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(44, 55);
+            this.txtSource.Multiline = true;
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(150, 67);
+            this.txtSource.TabIndex = 0;
+            // 
+            // txtTarget
+            // 
+            this.txtTarget.Location = new System.Drawing.Point(309, 55);
+            this.txtTarget.Multiline = true;
+            this.txtTarget.Name = "txtTarget";
+            this.txtTarget.Size = new System.Drawing.Size(150, 67);
+            this.txtTarget.TabIndex = 1;
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Location = new System.Drawing.Point(205, 146);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(94, 46);
+            this.btnTranslate.TabIndex = 2;
+            this.btnTranslate.Text = "Traducir";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // cmbSourceLanguage
+            // 
+            this.cmbSourceLanguage.FormattingEnabled = true;
+            this.cmbSourceLanguage.Location = new System.Drawing.Point(44, 28);
+            this.cmbSourceLanguage.Name = "cmbSourceLanguage";
+            this.cmbSourceLanguage.Size = new System.Drawing.Size(150, 21);
+            this.cmbSourceLanguage.TabIndex = 3;
+            // 
+            // cmbTargetLanguage
+            // 
+            this.cmbTargetLanguage.FormattingEnabled = true;
+            this.cmbTargetLanguage.Location = new System.Drawing.Point(309, 28);
+            this.cmbTargetLanguage.Name = "cmbTargetLanguage";
+            this.cmbTargetLanguage.Size = new System.Drawing.Size(150, 21);
+            this.cmbTargetLanguage.TabIndex = 4;
+            // 
+            // ReadSource
+            // 
+            this.ReadSource.Location = new System.Drawing.Point(44, 128);
+            this.ReadSource.Name = "ReadSource";
+            this.ReadSource.Size = new System.Drawing.Size(30, 23);
+            this.ReadSource.TabIndex = 5;
+            this.ReadSource.Text = "🔊";
+            this.ReadSource.UseVisualStyleBackColor = true;
+            this.ReadSource.Click += new System.EventHandler(this.ReadSource_Click);
+            // 
+            // ReadTarget
+            // 
+            this.ReadTarget.Location = new System.Drawing.Point(429, 128);
+            this.ReadTarget.Name = "ReadTarget";
+            this.ReadTarget.Size = new System.Drawing.Size(30, 23);
+            this.ReadTarget.TabIndex = 6;
+            this.ReadTarget.Text = "🔊";
+            this.ReadTarget.UseVisualStyleBackColor = true;
+            this.ReadTarget.Click += new System.EventHandler(this.ReadTarget_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 226);
+            this.Controls.Add(this.ReadTarget);
+            this.Controls.Add(this.ReadSource);
+            this.Controls.Add(this.cmbTargetLanguage);
+            this.Controls.Add(this.cmbSourceLanguage);
+            this.Controls.Add(this.btnTranslate);
+            this.Controls.Add(this.txtTarget);
+            this.Controls.Add(this.txtSource);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Traductor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtTarget;
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.ComboBox cmbSourceLanguage;
+        private System.Windows.Forms.ComboBox cmbTargetLanguage;
+        private System.Windows.Forms.Button ReadSource;
+        private System.Windows.Forms.Button ReadTarget;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace traductor
+{
+    public partial class Form1 : Form
+    {
+        //Estas líneas de código declaran variables relacionadas con la traducción utilizando la API de Google Translate
+        GoogleTranslateApi.Language source;
+        GoogleTranslateApi.Language target;
+        GoogleTranslateApi.GoogleTranslator translate;
+
+        //Sirve para habilitar la síntesis de voz en una aplicación.
+        private SpeechSynthesizer synthesizer;
+
+        //Sirve para mapear un nombre de idioma legible por humanos a su código de idioma correspondiente.
+        private string GetLanguageCode(string languageName)
+        {
+            //Es una implementacion switch (interruptor) que toma un nombre de idioma (languageName) como entrada y devuelve el código de idioma correspondiente.
+            switch (languageName) // Switch = Utilizada para realizar una selección condicional entre múltiples opciones o casos.
+            {
+                case "Portuguese":
+                    return "pt";
+                case "English":
+                    return "en";
+                case "Spanish":
+                    return "es";
+                case "Russian":
+                    return "ru";
+                case "French":
+                    return "fr";
+                case "German":
+                    return "de";
+                case "Swedish":
+                    return "sv";
+                case "Italian":
+                    return "it";
+                case "Japanese":
+                    return "ja";
+                case "Chinese":
+                    return "zh";
+                default:
+                    // En caso de que no se encuentre el idioma, puedes manejarlo de la forma que prefieras.
+                    // En este ejemplo, se devolverá una cadena vacía.
+                    return "";
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Sirve para preparar tu aplicación para que pueda generar voz a partir de texto.
+            synthesizer = new SpeechSynthesizer();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Definir idiomas 
+            source = GoogleTranslateApi.Language.Parse("es");
+            target = GoogleTranslateApi.Language.Parse("en");        
+
+            // Declarar e inicializar Traductor
+            translate = new GoogleTranslateApi.GoogleTranslator(
+                source,
+                target
+            );
+
+            // Llena el ComboBox de idioma fuente
+            cmbSourceLanguage.Items.Add("Spanish");
+            cmbSourceLanguage.Items.Add("Portuguese"); 
+            cmbSourceLanguage.Items.Add("English");
+            cmbSourceLanguage.Items.Add("Russian");
+            cmbSourceLanguage.Items.Add("French");
+            cmbSourceLanguage.Items.Add("German");
+            cmbSourceLanguage.Items.Add("Swedish");
+            cmbSourceLanguage.Items.Add("Italian");
+            cmbSourceLanguage.Items.Add("Japanese");
+            cmbSourceLanguage.Items.Add("Chinese");
+
+            // Llena el ComboBox de idioma de destino
+            cmbTargetLanguage.Items.Add("Portuguese");
+            cmbTargetLanguage.Items.Add("Spanish");
+            cmbTargetLanguage.Items.Add("English");
+            cmbTargetLanguage.Items.Add("Russian");
+            cmbTargetLanguage.Items.Add("French");
+            cmbTargetLanguage.Items.Add("German");
+            cmbTargetLanguage.Items.Add("Swedish");
+            cmbTargetLanguage.Items.Add("Italian");
+            cmbTargetLanguage.Items.Add("Japanese");
+            cmbTargetLanguage.Items.Add("Chinese");
+
+            // Establece una selección inicial si lo deseas
+            cmbSourceLanguage.SelectedIndex = 0;
+            cmbTargetLanguage.SelectedIndex = 1;
+        }
+
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            // Sirve para obtener y almacenar información introducida por el usuario
+            string textSource = txtSource.Text;
+            string selectedSourceLanguage = cmbSourceLanguage.SelectedItem.ToString();
+            string selectedTargetLanguage = cmbTargetLanguage.SelectedItem.ToString();
+
+            // Convierte los nombres de idiomas a códigos de idioma, por ejemplo, "Español" a "es".
+            GoogleTranslateApi.Language sourceLanguage = GoogleTranslateApi.Language.Parse(GetLanguageCode(selectedSourceLanguage));
+            GoogleTranslateApi.Language targetLanguage = GoogleTranslateApi.Language.Parse(GetLanguageCode(selectedTargetLanguage));
+
+            // Inicializa el traductor con los idiomas seleccionados
+            translate = new GoogleTranslateApi.GoogleTranslator(sourceLanguage, targetLanguage);
+
+            // Permite a los usuarios ingresar texto en un idioma, traducir ese texto a otro idioma y mostrar la traducción en la interfaz de usuario.
+            string textTarget = translate.Text(textSource);
+            txtTarget.Text = textTarget;
+        }
+
+        private void ReadSource_Click(object sender, EventArgs e)
+        {
+            // Habilita la síntesis de voz con el propósito de leer en voz alta el contenido de un control de caja de texto.
+            string textToRead = txtSource.Text;
+            synthesizer.SpeakAsync(textToRead);
+        }
+
+        private void ReadTarget_Click(object sender, EventArgs e)
+        {
+            // Habilita la síntesis de voz con el propósito de leer en voz alta el contenido de un control de caja de texto.
+            string textToRead = txtTarget.Text;
+            synthesizer.SpeakAsync(textToRead);
+        }
+    }
+}
